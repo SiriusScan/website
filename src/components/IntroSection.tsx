0e1edc34a5b93e46b50b1ae5e6d5c4405bb11c0f
Matthew Toussain <oz@sephiroth.local>
2023-10-07 19:54:11 -0500

    Intro

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -6,7 +6,7 @@ import "typeface-roboto-slab";
 
 const IntroSection: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
-  const title = useTypewriter("Welcome to Sirius Scan", 30);
+  const title = useTypewriter("Welcome to Sirius Scan");
 
   return (
     <div
